28d61489e8fbc4bf91c5a6f5ea01e8507eae45cf
Hussain Hassani <hussainhassani@hotmail.com>
2023-02-05 21:16:03 +0000

    implemented short circuit

--- src/conditional-rendering/2-short-circuit.js
+++ src/conditional-rendering/2-short-circuit.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 
 const ShortCircuit = () => {
-    const [text, setText] = useState('peter');
+    const [text, setText] = useState('');
+    const [isError, setIsError] = useState(false);
 
     const firstValue = text || 'hello world';
     const secondValue = text && 'hello world';
 
     return (
         <>
-            <h1>{firstValue}</h1>
-            <h1>value: {secondValue}</h1>
+            {/* <h1>{firstValue}</h1>
+            <h1>value: {secondValue}</h1> */}
+
+            <h1>{text || 'john doe'}</h1>
+            <button className="btn" onClick={() => setIsError(!isError)}>
+                toggle error
+            </button>
+
+            {isError && <h1>Error...</h1>}
+            {isError ? 
+                (<p>there is an error...</p>) : 
+                (<div>
+                    there is no error
+                </div>)}
         </>
     );
 };
