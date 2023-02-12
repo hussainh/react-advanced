f71c80342bd1fa37e11ad7299701ae910b14ac46
Hussain Hassani <hussainhassani@hotmail.com>
2023-02-12 22:59:26 +0000

    initial useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Setup from './useRef/basics'
+import Final from './useReducer'
 
 function App() {
   return (
     <div className="container">
-      <Setup />
+      <Final />
     </div>
   );
 }
---
+++ src/useReducer/Modal.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const Modal = () => {
+    return <div>this is a modal</div>;
+};
+
+export default Modal;
