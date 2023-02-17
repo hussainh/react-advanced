53e1a239b385230b57a957c805569f863a8a2313
Hussain Hassani <hussainhassani@hotmail.com>
2023-02-17 07:52:51 +0000

    refactoring reducer into its own js file

--- src/useReducer/index.js
+++ src/useReducer/index.js
@@ -1,51 +1,7 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../data";
-
-const reducer = (state, action) => {
-    if (action.type === "ADD_ITEM") {  
-        const newPerson = [...state.people, action.payload];    
-        return {            
-            ...state,
-            people: newPerson,
-            isModalOpen: true,
-            modalContent: 'item added'
-        };
-    }
-    
-    if (action.type === "NO_VALUE") {
-        return {
-            ...state,
-            isModalOpen: true,
-            modalContent: "please enter a value"
-        };
-    }
-
-    if (action.type === "CLOSE_MODAL") {
-        return {
-            ...state,
-            isModalOpen: false
-        };
-    }
-    
-    if (action.type === "REMOVE_ITEM") {
-        
-        const personName = action.payload.name;
-        
-        const newListOfPeople = state.people.filter( 
-            (person) => person.id !== action.payload.id
-        );
-
-        return {
-            ...state,
-            isModalOpen: true,
-            modalContent: personName + " removed",
-            people: newListOfPeople
-        };
-    }
-
-    throw new Error("no matching action type");
-};
+import { reducer } from './reducer'
 
 const defaultState = {
     people: [],
