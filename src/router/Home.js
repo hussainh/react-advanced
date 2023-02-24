24735000400826503414c67120550c29c2b3e3b4
Hussain Hassani <hussainhassani@hotmail.com>
2023-02-24 23:41:20 +0000

    temp

--- src/router/Home.js
+++ src/router/Home.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+            <p>shake and bake</p>
+        </div>
     );
 };
 
