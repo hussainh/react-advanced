601645efaf2a0ca8d0d07dd43d26ae233adf7a90
Hussain Hassani <hussainhassani@hotmail.com>
2023-03-02 07:29:42 +0000

    finished creating person page

--- src/router/Person.js
+++ src/router/Person.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { data } from "../data";
 import { Link, useParams } from "react-router-dom";
 
 const Person = () => {
+    const { id } = useParams();
+    const [ name, setName ] = useState("default name");
+
+    useEffect(() => {
+        const selectedPerson = data.find((person) => person.id === parseInt(id));
+        setName(selectedPerson.name);
+    }, []);
+
     return (
         <div>
-            <h2>person</h2>
+            <h1>{name}</h1>
+            <Link to="/people" className="btn">Back to people</Link>
         </div>
     );
 };
