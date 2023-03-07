a45d826001b5d8be43fee0226d08346e26f7f2d9
Hussain Hassani <hussainhassani@hotmail.com>
2023-03-07 08:27:39 +0000

    implemented useCallback on products page

--- src/useMemo-useCallback/index.js
+++ src/useMemo-useCallback/index.js
@@ -6,6 +6,11 @@ const url = "https://course-api.com/javascript-store-products";
 const Index = () => {
     const { products } = useFetch(url);
     const [count, setCount] = useState(0);
+    const [cart, setCart] = useState(0);
+
+    const addToCart = useCallback(() => {
+        setCart(cart + 1);
+    }, [cart]);
 
     return (
         <>
@@ -17,12 +22,14 @@ const Index = () => {
                 Increment count
             </button>
 
-            <BigList products = {products}/>
+            <h1 style={{marginTop: "3rem"}}>Cart: { cart }</h1>
+            
+            <BigList products = {products} addToCart={addToCart} />
         </>
     );
 };
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
     useEffect(() => {
         console.log("big list called");
     });
@@ -30,13 +37,18 @@ const BigList = React.memo(({ products }) => {
     return (
         <section className="products">
             {products.map((product) => {
-                return <SingleProduct key={product.id} {...product}></SingleProduct>;
+                return (
+                    <SingleProduct 
+                        key={product.id} 
+                        {...product} 
+                        addToCart={addToCart}>
+                    </SingleProduct>);
             })}
         </section>
     );
 });
 
-const SingleProduct = ({fields}) => {
+const SingleProduct = ({fields, addToCart}) => {
         
     useEffect(() => {
         console.log("single item called");
@@ -45,12 +57,14 @@ const SingleProduct = ({fields}) => {
     let { name, price } = fields;
     price = price / 100;
 
-    const image = fields.image[0].url;
+    // commenting images out as we are getting failed certificate
+    //const image = fields.image[0].url;
     return (
         <article className="product">
-            <img src={image} alt={name} />
+            {/* <img src={image} alt={name} /> */}
             <h4>{name}</h4>
             <p>${price}</p>
+            <button onClick={addToCart}>Add to cart</button>
         </article>
     );
 };
